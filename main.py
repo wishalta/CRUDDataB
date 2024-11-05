d065e2ace7aaab6cb9923c166d9fdcf0ffcdb3f2
wishalta <rolandas.lebed@gmail.com>
2024-11-05 11:25:47 +0200

    CRUD first 5 done, output as well

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def print_info():
     print("2. Naujos Prekės įkėlimas")
     print("3. Prekės redagavimas")
     print("4. Prekės šalinimas")
-    print("5. Populiariausia prekė")
+    print("5. Pirkti preke")
     print("6. Daugiausiai pelno sugeneravusi prekė")
     print("7. Ataskaita periode")
     print("8. Išaldyti pinigai")
@@ -25,10 +25,11 @@ def print_info():
     print("-----------------------------------------------")
 
 def print_items():
-    query = "select * from dmo_items"
+    query = "select `id`, `title`, `manufacturer_price`, `sale_price`, `quantity` from dmo_items"
     c.execute(query)
     result = c.fetchall()
-    print(result)
+    for item in result:
+        print(f'id:{item[0]}, product: {item[1]}, cost: {item[2]}, buyer_cost: {item[3]}, quantity: {item[4]}')
 
 def get_item(id):
     query = "select * from dmo_items where id = " + id
@@ -86,6 +87,61 @@ def delete_item():
     c.execute(query, (id))
     conn.commit()
 
+# def transaction(quantity2):
+#     id = which_id()
+#     item = get_item(id)
+#     print('-------------------------------------------------')
+#     print(item)
+#     for i in range(0,100):
+#         if quantity2 <= item['quantity']:
+#             item['quantity'] = item['quantity'] - quantity2
+#             print(item['quantity'])
+#             print(F'Sekmingai isigijote {quantity2} vienetu/-us')
+#             return item['quantity']
+#         elif quantity2 > item['quantity']:
+#             print('Tiek vienetu neturim')
+#             print(f'Turime {item['quantity']} vienetu')
+#             print('Kiek noretumet pirkti?')
+#             many = int(input())
+#             item['quantity'] = item['quantity'] - many
+#             return item ['quantity']
+#
+# def which_id():
+#     print('Kuria preke perkate?')
+#     id = input()
+#     # return id
+
+def buy_item():
+    print('Kuria preke perkate?')
+    id = input()
+    item = get_item(id)
+    print(item)
+    print('Kiek perkate vienetu?')
+    many = int(input())
+    for i in range(0,100):
+        if many <= item['quantity']:
+            item['quantity'] = item['quantity'] - many
+            print(item['quantity'])
+            print(F'Sekmingai isigijote {many} vienetu/-us')
+            # return item['quantity']
+            break
+        elif many > item['quantity']:
+            print('Tiek vienetu neturim')
+            print(f'Turime {item['quantity']} vienetu')
+            print('Kiek noretumet pirkti?')
+            many = int(input())
+            item['quantity'] = item['quantity'] - many
+            if many > item['quantity']:
+                continue
+            if many <= item['quantity']:
+                print(f'Sekmingai isigijote {many} vienetu/-us')
+                break
+    query = (F"UPDATE `dmo_items` SET `quantity`= %s WHERE `id`=%s")
+    print(query)
+    c.execute(query, (item['quantity'], id))
+    conn.commit()
+
+
 while True:
     print_info()
     opt = input()
@@ -100,4 +156,6 @@ while True:
         case '4':
             delete_item()
         case '5':
+            buy_item()
+        case '6':
             exit(1)
